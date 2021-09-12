66ce3caeb2375fde333b8de8266e0af86294e99e
Markus Hauck <markus1189@gmail.com>
2021-09-12 10:11:38 +0200

    Increase delay

--- dog-bot.sh
+++ dog-bot.sh
@@ -3,6 +3,8 @@
 #!nix-shell -i bash
 #!nix-shell shell.nix
 
+SEND_DELAY="10s"
+
 getLinksForYesterday() {
     curl --retry 3 -s 'https://www.tiervermittlung.de/cgi-bin/haustier/db.cgi?db=hunde5&uid=default&ID=&Tierart=Hund&Rasse=&Groesse=&Geschlecht=weiblich&Alter-gt=3&Alter-lt=15.1&Zeitwert=Monate&Titel=&Name=&Staat=&Land=&PLZ=&PLZ-gt=&PLZ-lt=&Ort=&Grund=&Halter=&Notfall=&Chiffre=&keyword=&Date=&referer=&Nachricht=&E1=&E2=&E3=&E4=&E5=&E6=&E7=&E8=&E9=&E10=&mh=100&sb=0&so=descend&ww=&searchinput=&layout=&session=kNWVQkHlAVH5axV0HJs5&Bild=&video_only=&String_Rasse=&view_records=Suchen' | pup --charset iso-8859-1  --color '#Item_Results json{}' | jq -r '(now | todate | fromdate - 86400 | gmtime | strftime("%d.%m.%Y")) as $yesterday | map({date: .children[1].text, link: .children[0].children[0].children[0].href} | select(.date == $yesterday) | .link)[]'
 }
@@ -14,6 +16,7 @@ postLink() {
 
     FLAGS=$(
         cd "${TEMP}" || return
+
         echo "${LINKS}" | parallel -j6 -- wget --quiet
         find . -type f | while read -r i; do echo "-F $(basename "$i")=@$(pwd)/$i"; done | paste -s -d ' '
          )
@@ -37,7 +40,7 @@ postLink() {
          -F disable_notification=true \
          ${FLAGS}
     echo
-    sleep 3
+    sleep "${SEND_DELAY}"
 
     echo "${VIDEO_URIS}" | while read -r video; do
         if [[ -z "${video}" ]]; then
@@ -46,14 +49,14 @@ postLink() {
             echo "Sending video"
             curl --retry 3 -s -F disable_notification=true -F video="${video}" "https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendVideo?chat_id=${TELEGRAM_CHAT_ID}"
             echo
-            sleep 3
+            sleep "${SEND_DELAY}"
         fi
     done
 
     echo "Sending link"
     curl --retry 3 -s "https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage?chat_id=${TELEGRAM_CHAT_ID}&text=${1}&disable_notification=true"
     echo
-    sleep 3
+    sleep "${SEND_DELAY}"
 
     # Cleanup TEMP
     rm "${TEMP}"/* && rmdir "${TEMP}"
